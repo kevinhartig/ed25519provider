56deaad85fdfc1c1a57378b6d145514c6cf65c24
khartig <kevin@fiseportal.com>
2023-08-02 21:38:35 -0700

    remove files from origin

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Getting Started with Create React App
+# Simple example of passing messages with ComposeDB
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### `npm ceramic`
+
+Starts a local Ceramic node. Ceramic and CeramicDB must be installed to use the example.
+e.g.
+$ mkdir composedb && cd composedb
+$ npm init -y
+$ npm i @ceramicnetwork/cli @ceramicnetwork/http-client @composedb/cli @composedb/devtools @composedb/devtools-node dids key-did-provider-ed25519 key-did-resolver uint8arrays
+  
+Additional info is at
+https://composedb.js.org
+https://dev.to/fllstck/decentralized-databases-composedb-49m3
 
 ## Available Scripts
 
@@ -38,33 +48,3 @@ If you aren't satisfied with the build tool and configuration choices, you can `
 Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
 You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,51 +1,165 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import { Ed25519Provider } from "key-did-provider-ed25519";
+import KeyResolver from "key-did-resolver";
+import { DID } from "dids";
+import seedrandom from "seedrandom";
+import { CeramicClient } from "@ceramicnetwork/http-client";
+import { ComposeClient } from "@composedb/client";
+import { definition } from "./__generated__/definition.js";
 
-import {Ed25519Provider} from 'key-did-provider-ed25519'
-import KeyResolver from 'key-did-resolver'
-import {DID} from 'dids'
-
-import seedrandom from 'seedrandom'
+const ceramic = new CeramicClient("http://localhost:7007");
+const composeClient = new ComposeClient({
+    ceramic: "http://localhost:7007",
+    // cast our definition as a RuntimeCompositeDefinition
+    definition,
+});
 
 const App = () => {
-    const [randomDid, setRandomDid] = useState('')
-    const [staticDid, setStaticDid] = useState('')
+    const [randomD, setRandomDid] = useState("");
+    const [otherD, setOtherDid] = useState("");
+    const [staticD, setStaticDid] = useState("");
+    const [comment, setComment] = useState("");
+    const [decrypted, setDecrypted] = useState("");
 
     const createRandomDID = async () => {
-        const seed = new Uint8Array(32)
-        crypto.getRandomValues(seed)
-        const randomDid = new DID({
-            provider: new Ed25519Provider(seed),
-            resolver: KeyResolver.getResolver()
-        })
-        await randomDid.authenticate()
-
-        setRandomDid(randomDid.id)
+        if (!randomD) {
+            console.log(randomD);
+            const seed = new Uint8Array(32);
+            crypto.getRandomValues(seed);
+            const randomDid = new DID({
+                provider: new Ed25519Provider(seed),
+                resolver: KeyResolver.getResolver(),
+            });
+            await randomDid.authenticate();
+            setRandomDid(randomDid.id);
+        } else {
+            alert("Already set");
+        }
     };
 
     const createStaticDID = async () => {
-        const uniqueKey = '0x7a8b9cde'
-        const rng = seedrandom(uniqueKey)
-        const seed = new Uint8Array(32)
+        console.log(staticD);
+        const uniqueKey = "0x7a8b9cde";
+        const rng = seedrandom(uniqueKey);
+        const seed = new Uint8Array(32);
         for (let i = 0; i < 32; i += 1) {
-            seed[i] = Math.floor(rng() * 256)
+            seed[i] = Math.floor(rng() * 256);
         }
         const staticDid = new DID({
             provider: new Ed25519Provider(seed),
-            resolver: KeyResolver.getResolver()
-        })
-        await staticDid.authenticate()
+            resolver: KeyResolver.getResolver(),
+        });
+        await staticDid.authenticate();
+        //authenticate on ceramic instance
+        composeClient.setDID(staticDid);
+        ceramic.did = staticDid;
 
-        setStaticDid(staticDid.id)
+        setStaticDid(staticDid.id);
+        return staticDid;
+    };
+
+    const createOtherDID = async () => {
+        const uniqueKey = "0x5p2z3def";
+        const rng = seedrandom(uniqueKey);
+        const seed = new Uint8Array(32);
+        for (let i = 0; i < 32; i += 1) {
+            seed[i] = Math.floor(rng() * 256);
+        }
+        const otherDid = new DID({
+            provider: new Ed25519Provider(seed),
+            resolver: KeyResolver.getResolver(),
+        });
+        await otherDid.authenticate();
+
+        setOtherDid(otherDid.id);
+        return otherDid;
+    };
+
+    const create = async () => {
+        const staticDid = await createStaticDID();
+        const cleartext = "this is a secret message";
+        const jwe = await staticDid.createDagJWE(cleartext, [otherD]);
+        //stringify your JWE object and replace escape characters
+        const stringified = JSON.stringify(jwe).replace(/"/g, "`");
+        if (staticD !== undefined) {
+            const update = await composeClient.executeQuery(`
+          mutation{
+            createPost(
+            input: {
+                content: {
+                text: "${stringified}"
+                }
+            }
+            ) {
+            document {
+              id
+              text
+              author{
+                id
+                }
+            }
+            }
+            }
+          `);
+            console.log('In create update value from query = ' + update);
+            const item = update.data?.createPost?.document?.text;
+            setComment(item)
+        }
+    };
+
+    const decrypt = async () => {
+
+        const query = await composeClient.executeQuery(`
+      query{
+        postIndex(last:1){
+          edges{
+            node{
+              text
+            }
+          }
+        }
+      }
+      `);
+        console.log('In decrypt update value from query = ' + query)
+        const arr = query.data?.postIndex?.edges;
+
+        //Reverse-replacement of backticks for double-quotes prior to parsing
+        const string = arr[0].node.text.replace(/`/g, '"');
+
+        const other = await createOtherDID();
+        console.log(other)
+        await other.authenticate();
+
+        const plaintext = await other.decryptDagJWE(JSON.parse(string));
+        console.log(plaintext)
+        setDecrypted(plaintext)
     };
 
     return (
         <div>
             <h1>Create DID</h1>
             <button onClick={createRandomDID}>Random DID</button>
-            <p>DID: {randomDid}</p>
-            <br/>
+            <p>DID: {randomD}</p>
+            <br />
+            <br />
+            <br />
+            <h2>1. Create Static DID</h2>
             <button onClick={createStaticDID}>Static DID</button>
-            <p>DID: {staticDid}</p>
+            <p>DID: {staticD}</p>
+            <br />
+            <h2>2. Create Other DID</h2>
+            <p>DID: {otherD}</p>
+            <button onClick={createOtherDID}>Other DID</button>
+            <br />
+            <br />
+            {otherD &&<h2>3. Create a Comment</h2>}
+            {otherD &&<button onClick={create}>Create Comment</button>}
+            {otherD && <p>Encrypted Comment: {comment}</p>}
+            <br />
+            <br />
+            {otherD && <h2>4. Decrypt as Other DID</h2>}
+            {otherD && <button onClick={decrypt}>Decrypt as Other DID</button>}
+            {otherD && <p>Decrypted Comment: <br/> <br/> {decrypted}</p>}
         </div>
     );
 };
